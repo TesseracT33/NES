84e004f62153997bb060b7829384176dfebb52ba
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2021-11-06 20:22:40 +0100

    Make BitUtils functions templated

--- src/BitUtils.h
+++ src/BitUtils.h
@@ -1,24 +1,46 @@
 #pragma once
 
-#include "Types.h"
+#include <concepts>
 
 namespace BitUtils
 {
-	inline bool CheckBit(u8& byte, u8 pos) { return byte & (1u << pos); }
-	inline bool CheckBit(u16& word, u8 pos) { return word & (1u << pos); }
-	inline void SetBit(u8& byte, u8 pos) { byte |= (1u << pos); }
-	inline void SetBit(u16& word, u8 pos) { word |= (1u << pos); }
-	inline void ClearBit(u8& byte, u8 pos) { byte &= ~(1u << pos); }
-	inline void ClearBit(u16& word, u8 pos) { word &= ~(1u << pos); }
-	inline void ToggleBit(u8& byte, u8 pos) { byte ^= ~(1u << pos); }
-	inline void ToggleBit(u16& word, u8 pos) { word ^= ~(1u << pos); }
+	template<std::integral T> inline constexpr bool CheckBit(T num, unsigned pos)
+	{
+		return num & (1ull << pos);
+	}
 
-	inline bool CheckBit(u8* byte, u8 pos) { return *byte & (1u << pos); }
-	inline bool CheckBit(u16* word, u8 pos) { return *word & (1u << pos); }
-	inline void SetBit(u8* byte, u8 pos) { *byte |= (1u << pos); }
-	inline void SetBit(u16* word, u8 pos) { *word |= (1u << pos); }
-	inline void ClearBit(u8* byte, u8 pos) { *byte &= ~(1u << pos); }
-	inline void ClearBit(u16* word, u8 pos) { *word &= ~(1u << pos); }
-	inline void ToggleBit(u8* byte, u8 pos) { *byte ^= ~(1u << pos); }
-	inline void ToggleBit(u16* word, u8 pos) { *word ^= ~(1u << pos); }
+	template<std::integral T> inline constexpr bool CheckBit(T* num, unsigned pos)
+	{
+		return *num & (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void SetBit(T& num, unsigned pos)
+	{
+		num |= (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void SetBit(T* num, unsigned pos)
+	{
+		*num |= (1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ClearBit(T& num, unsigned pos)
+	{
+		num &= ~(1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ClearBit(T* num, unsigned pos)
+	{
+		*num &= ~(1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ToggleBit(T& num, unsigned pos)
+	{
+		num ^= ~(1ull << pos);
+	}
+
+	template<std::integral T> inline constexpr void ToggleBit(T* num, unsigned pos)
+	{
+		*num ^= ~(1ull << pos);
+	}
 }
