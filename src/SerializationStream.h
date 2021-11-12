9bde5f9d23421d702c205ff4f1c1dc325972477b
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2021-11-12 16:06:37 +0100

    SerializationStream: implement move constructor, delete copy constructor, implement StreamBitField function, fix StreamArray function

--- src/SerializationStream.h
+++ src/SerializationStream.h
@@ -6,14 +6,18 @@
 #include <string>
 #include <vector>
 
-// Used for save states and configuration files; reads or writes data from/to files
+/* A class used for save states and configuration files; reads or writes data from/to files */
 class SerializationStream
 {
 private:
+	bool has_error = false; // an error has occured while reading/writing.
 	std::fstream fstream;
 
 	void Stream(void* obj, size_t size)
 	{
+		if (has_error)
+			return;
+
 		if (mode == Mode::Serialization)
 			fstream.write((const char*)obj, size);
 		else
@@ -21,14 +25,12 @@ private:
 
 		if (!fstream)
 		{
-			error = true; // todo; implement better error handling
+			has_error = true; // todo; implement better error handling
 		}
 	}
 
 public:
 	enum class Mode { Serialization, Deserialization } const mode;
-
-	bool error = false; // an error has occured while reading/writing.
 
 	SerializationStream(const char* file_path, Mode mode) : SerializationStream(std::string(file_path), mode) {}
 
@@ -42,13 +44,47 @@ public:
 
 		if (!fstream)
 		{
-			error = true;
+			has_error = true;
 		}
+	}
+
+	SerializationStream(const SerializationStream& other) = delete;
+
+	SerializationStream(SerializationStream&& other) noexcept : mode(other.mode)
+	{
+		*this = std::move(other);
+	}
+
+	SerializationStream& operator=(const SerializationStream& other) = delete;
+
+	SerializationStream& operator=(SerializationStream&& other) noexcept
+	{
+		if (this != &other)
+		{
+			this->fstream.swap(other.fstream);
+			this->has_error = other.has_error;
+			other.has_error = false;
+		}
+		return *this;
+	}
+
+	bool HasError() const
+	{
+		return has_error;
 	}
 
 	template<typename T> void StreamPrimitive(T& number)
 	{
 		Stream(&number, sizeof T);
+	}
+
+	/* We cannot take the address of (pointer/ref) of a bitfield.
+	   So take it by copy, read/write from/to it, and then return it.
+	   The caller will need to make sure that the result is not discarded. */
+	template<typename T> [[nodiscard]] T StreamBitfield(T number)
+	{
+		Stream(&number, sizeof T);
+		return number;
 	}
 
 	void StreamString(std::string& str)
@@ -73,19 +109,7 @@ public:
 
 	template<typename T, size_t size> void StreamArray(std::array<T, size>& array)
 	{
-		if (mode == Mode::Serialization)
-		{
-			Stream(&size, sizeof(size_t));
-			for (T t : array)
-				Stream(&t, sizeof(T));
-		}
-		else
-		{
-			size_t new_size = 0;
-			Stream(&new_size, sizeof(size_t));
-			array = std::array<T, new_size>();
-			Stream(&array[0], sizeof(T) * new_size);
-		}
+		Stream(&array[0], sizeof(T) * size);
 	}
 
 	template<typename T> void StreamArray(T* array, size_t size)
