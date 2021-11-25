31d4c05cbbd319d6f4fef5d657ab7bcbcf5b6d08
TesseracT33 <christoffer.ekgrim98@hotmail.com>
2021-11-25 22:28:59 +0100

    Fix MMC3 bugs; some optimizations

--- src/core/mappers/MMC3.h
+++ src/core/mappers/MMC3.h
@@ -18,8 +18,8 @@ public:
 		case 0x6: case 0x7:
 			// Disabling PRG RAM causes reads from the PRG RAM region to return open bus.
 			if (properties.has_prg_ram && prg_ram_enabled)
-				return value_last_read_from_prg_ram = prg_ram[addr - 0x6000];
-			return value_last_read_from_prg_ram;
+				return prg_ram_open_bus = prg_ram[addr - 0x6000];
+			return prg_ram_open_bus;
 
 		// CPU $8000-$9FFF (or $C000-$DFFF): 8 KiB switchable PRG ROM bank
 		case 0x8: case 0x9:
@@ -58,15 +58,16 @@ public:
 
 		// CPU $8000-$9FFF; bank select (even), bank data (odd)
 		case 0x8: case 0x9:
-			if (addr & 0x01)
+			if (addr & 1)
 			{
-				rom_bank[bank_reg_select] = data;
-				/* R0 and R1 ignore the bottom bit, as the value written still counts banks in 1KB units but odd numbered banks can't be selected. */
+				rom_bank[bank_reg_select] = data; 
+				/* MMC3 has 8 CHR address lines, and the CHR capacity is always 256K, so no need to mod with the number of available banks, if a CHR bank was chosen.
+				   R0 and R1 ignore the bottom bit, as the value written still counts banks in 1KB units but odd numbered banks can't be selected. */
 				if (bank_reg_select == 0 || bank_reg_select == 1)
-					rom_bank[bank_reg_select] &= ~0x01;
-				/* R6 and R7 will ignore the top two bits, as the MMC3 has only 6 PRG ROM address lines. */
+					rom_bank[bank_reg_select] &= 0xFE;
+				/* R6 and R7 (PRG) will ignore the top two bits, as MMC3 has only 6 PRG ROM address lines. */
 				else if (bank_reg_select == 6 || bank_reg_select == 7)
-					rom_bank[bank_reg_select] &= ~0xC0;
+					rom_bank[bank_reg_select] &= 0x3F;
 			}
 			else
 			{
@@ -78,9 +79,9 @@ public:
 
 		// CPU $A000-$BFFF; mirroring (even), PRG RAM protect (odd)
 		case 0xA: case 0xB:
-			if (addr & 0x01)
+			if (addr & 1)
 			{
-				prg_ram_write_protection = data & 0x40;
+				prg_ram_write_protection = data & 0x40; // (0: allow writes; 1: deny writes)
 				prg_ram_enabled = data & 0x80;
 			}
 			else
@@ -93,15 +94,19 @@ public:
 
 		// CPU $C000-$DFFF; IRQ latch (even), IRQ reload (odd)
 		case 0xC: case 0xD:
-			if (addr & 0x01)
+			if (addr & 1)
+			{
+				IRQ_counter = 0;
 				reload_IRQ_counter_on_next_clock = true;
+			}
 			else
 				IRQ_counter_reload = data;
 			break;
 
 		// CPU $E000-$FFFF: IRQ disable (even), IRQ enable (odd)
 		case 0xE: case 0xF:
-			IRQ_enabled = addr & 0x01;
+			IRQ_enabled = addr & 1;
+			// TODO Writing any value to this register will disable MMC3 interrupts AND acknowledge any pending interrupts.
 			break;
 
 		default:
@@ -126,55 +131,56 @@ public:
 		   0: two 2 KB banks at $0000-$0FFF, four 1 KB banks at $1000-$1FFF;
 		   1: two 2 KB banks at $1000-$1FFF, four 1 KB banks at $0000-$0FFF.
 		*/
-		switch (addr / 0x400)
+		switch (addr >> 8)
 		{
 		// PPU $0000-$07FF (or $1000-$17FF): 2 KiB switchable CHR bank
-		case 0:
+		case 0x00: case 0x01: case 0x02: case 0x03:
 			if (chr_a12_inversion == 0)
-				return chr[addr + 0x800 * rom_bank[0]];
+				return chr[addr + 0x400 * rom_bank[0]];
 			return chr[addr + 0x400 * rom_bank[2]];
 		
-		case 1:
+		case 0x04: case 0x05: case 0x06: case 0x07:
 			if (chr_a12_inversion == 0)
-				return chr[addr + 0x800 * rom_bank[0]];
+				return chr[addr + 0x400 * rom_bank[0]];
 			return chr[addr + 0x400 * rom_bank[3] - 0x400];
 
 		// PPU $0800-$0FFF (or $1800-$1FFF): 2 KiB switchable CHR bank
-		case 2:
+		case 0x08: case 0x09: case 0x0A: case 0x0B:
 			if (chr_a12_inversion == 0)
-				return chr[addr + 0x800 * rom_bank[1] - 0x800];
+				return chr[addr + 0x400 * rom_bank[1] - 0x800];
 			return chr[addr + 0x400 * rom_bank[4] - 0x800];
 
-		case 3:
+		case 0x0C: case 0x0D: case 0x0E: case 0x0F:
 			if (chr_a12_inversion == 0)
-				return chr[addr + 0x800 * rom_bank[1] - 0x800];
+				return chr[addr + 0x400 * rom_bank[1] - 0x800];
 			return chr[addr + 0x400 * rom_bank[5] - 0xC00];
 
 		// PPU $1000-$13FF (or $0000-$03FF): 1 KiB switchable CHR bank
-		case 4:
+		case 0x10: case 0x11: case 0x12: case 0x13:
 			if (chr_a12_inversion == 0)
 				return chr[addr + 0x400 * rom_bank[2] - 0x1000];
-			return chr[addr + 0x800 * rom_bank[0] - 0x1000];
+			return chr[addr + 0x400 * rom_bank[0] - 0x1000];
 
 		// PPU $1400-$17FF (or $0400-$07FF): 1 KiB switchable CHR bank
-		case 5:
+		case 0x14: case 0x15: case 0x16: case 0x17:
 			if (chr_a12_inversion == 0)
 				return chr[addr + 0x400 * rom_bank[3] - 0x1400];
-			return chr[addr + 0x800 * rom_bank[0] - 0x1000];
+			return chr[addr + 0x400 * rom_bank[0] - 0x1000];
 
 		// PPU $1800-$1BFF (or $0800-$0BFF): 1 KiB switchable CHR bank
-		case 6:
+		case 0x18: case 0x19: case 0x1A: case 0x1B:
 			if (chr_a12_inversion == 0)
 				return chr[addr + 0x400 * rom_bank[4] - 0x1800];
-			return chr[addr + 0x800 * rom_bank[1] - 0x1800];
+			return chr[addr + 0x400 * rom_bank[1] - 0x1800];
 
 		// PPU $1C00-$1FFF (or $0C00-$0FFF): 1 KiB switchable CHR bank
-		case 7:
+		case 0x1C: case 0x1D: case 0x1E: case 0x1F:
 			if (chr_a12_inversion == 0)
 				return chr[addr + 0x400 * rom_bank[5] - 0x1C00];
-			return chr[addr + 0x800 * rom_bank[1] - 0x1800];
+			return chr[addr + 0x400 * rom_bank[1] - 0x1800];
 
-		default: return 0xFF; // impossible
+		default:
+			return 0xFF;
 		}
 	}
 
@@ -183,63 +189,64 @@ public:
 		if (!properties.has_chr_ram)
 			return;
 
-		switch (addr / 0x400)
+		switch (addr >> 8)
 		{
-			// PPU $0000-$07FF (or $1000-$17FF): 2 KiB switchable CHR bank
-		case 0:
+		// PPU $0000-$07FF (or $1000-$17FF): 2 KiB switchable CHR bank
+		case 0x00: case 0x01: case 0x02: case 0x03:
 			if (chr_a12_inversion == 0)
-				chr[addr + 0x800 * rom_bank[0]] = data;
+				chr[addr + 0x400 * rom_bank[0]] = data;
 			chr[addr + 0x400 * rom_bank[2]] = data;
 			break;
 
-		case 1:
+		case 0x04: case 0x05: case 0x06: case 0x07:
 			if (chr_a12_inversion == 0)
-				chr[addr + 0x800 * rom_bank[0]] = data;
+				chr[addr + 0x400 * rom_bank[0]] = data;
 			chr[addr + 0x400 * rom_bank[3] - 0x400] = data;
 			break;
 
-			// PPU $0800-$0FFF (or $1800-$1FFF): 2 KiB switchable CHR bank
-		case 2:
+		// PPU $0800-$0FFF (or $1800-$1FFF): 2 KiB switchable CHR bank
+		case 0x08: case 0x09: case 0x0A: case 0x0B:
 			if (chr_a12_inversion == 0)
-				chr[addr + 0x800 * rom_bank[1] - 0x800] = data;
+				chr[addr + 0x400 * rom_bank[1] - 0x800] = data;
 			chr[addr + 0x400 * rom_bank[4] - 0x800] = data;
 			break;
 
-		case 3:
+		case 0x0C: case 0x0D: case 0x0E: case 0x0F:
 			if (chr_a12_inversion == 0)
-				chr[addr + 0x800 * rom_bank[1] - 0x800] = data;
+				chr[addr + 0x400 * rom_bank[1] - 0x800] = data;
 			chr[addr + 0x400 * rom_bank[5] - 0xC00] = data;
 			break;
 
-			// PPU $1000-$13FF (or $0000-$03FF): 1 KiB switchable CHR bank
-		case 4:
+		// PPU $1000-$13FF (or $0000-$03FF): 1 KiB switchable CHR bank
+		case 0x10: case 0x11: case 0x12: case 0x13:
 			if (chr_a12_inversion == 0)
 				chr[addr + 0x400 * rom_bank[2] - 0x1000] = data;
-			chr[addr + 0x800 * rom_bank[0] - 0x1000] = data;
+			chr[addr + 0x400 * rom_bank[0] - 0x1000] = data;
 			break;
 
-			// PPU $1400-$17FF (or $0400-$07FF): 1 KiB switchable CHR bank
-		case 5:
+		// PPU $1400-$17FF (or $0400-$07FF): 1 KiB switchable CHR bank
+		case 0x14: case 0x15: case 0x16: case 0x17:
 			if (chr_a12_inversion == 0)
 				chr[addr + 0x400 * rom_bank[3] - 0x1400] = data;
-			chr[addr + 0x800 * rom_bank[0] - 0x1000] = data;
+			chr[addr + 0x400 * rom_bank[0] - 0x1000] = data;
 			break;
 
-			// PPU $1800-$1BFF (or $0800-$0BFF): 1 KiB switchable CHR bank
-		case 6:
+		// PPU $1800-$1BFF (or $0800-$0BFF): 1 KiB switchable CHR bank
+		case 0x18: case 0x19: case 0x1A: case 0x1B:
 			if (chr_a12_inversion == 0)
 				chr[addr + 0x400 * rom_bank[4] - 0x1800] = data;
-			chr[addr + 0x800 * rom_bank[1] - 0x1800] = data;
+			chr[addr + 0x400 * rom_bank[1] - 0x1800] = data;
 			break;
 
-			// PPU $1C00-$1FFF (or $0C00-$0FFF): 1 KiB switchable CHR bank
-		case 7:
+		// PPU $1C00-$1FFF (or $0C00-$0FFF): 1 KiB switchable CHR bank
+		case 0x1C: case 0x1D: case 0x1E: case 0x1F:
 			if (chr_a12_inversion == 0)
 				chr[addr + 0x400 * rom_bank[5] - 0x1C00] = data;
-			chr[addr + 0x800 * rom_bank[1] - 0x1800] = data;
+			chr[addr + 0x400 * rom_bank[1] - 0x1800] = data;
 			break;
 
-		default: break; // impossible
+		default:
+			break;
 		}
 	}
 
@@ -252,7 +259,6 @@ public:
 
 	void ClockIRQ() override
 	{
-		/* TODO: unsure about timing; should IRQ be triggered immediately when the counter becomes 0?*/
 		if (IRQ_counter == 0 && IRQ_enabled)
 			nes->cpu->SetIRQLow(IRQ_MMC3_mask);
 
@@ -295,8 +301,8 @@ protected:
 
 	u8 IRQ_counter = 0;
 	u8 IRQ_counter_reload = 0;
-	u8 value_last_read_from_prg_ram = 0;
-	u8 rom_bank[8]{}; // 0..5 : CHR.    6, 7 : PRG
+	u8 prg_ram_open_bus = 0;
+	u8 rom_bank[8]{}; // 0..5 : CHR; 6, 7 : PRG
 
 private:
 	static MapperProperties MutateProperties(MapperProperties properties)
